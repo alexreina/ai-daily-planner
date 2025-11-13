dda3c2abb25484d3f8b3adaa015b4365442e019f
alexreina <alex.reina@gmail.com>
2025-11-13 20:00:54 +0100

    Force latexmk rebuild in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-latexmk -xelatex -interaction=nonstopmode -halt-on-error ai-planner.tex
-
+# -g forces latexmk to rebuild even if it thinks outputs are up to date.
+latexmk -g -xelatex -interaction=nonstopmode -halt-on-error ai-planner.tex
